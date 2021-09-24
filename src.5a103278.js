96996f0f0d637f7621833b2280c350900161bb38
GrygoriyChernish <63448238+GrygoriyChernish@users.noreply.github.com>
2021-09-24 17:57:17 +0000

    Deploying to gh-pages from @ GrygoriyChernish/test-parcel-template@060025219eaf48eb478f40e3b92e823d83a53746 🚀

--- src.5a103278.js
+++ src.5a103278.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/test-parcel-template/src.afab55f3.js.map
+//# sourceMappingURL=/test-parcel-template/src.5a103278.js.map
